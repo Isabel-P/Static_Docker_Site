4ac76445d8fb3d2962175ce14de83ce4669468f7
Isabel-P <izzyschoolwork@gmail.com>
2022-12-22 20:48:46 -0500

    Server implmentation

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const path    = require('path');
 const server  = express();
 const port    = 3000;//port the server will run on
 server.use(express.json()); //allows for parsing into jason
+
+server.listen (port, () => console.log ('server is now listening on port '+ port)); //on and listening for request
+
+server.get('/', (req, res) => {
+    res.json({"message": "Docker Request Sent. Have a nice day."})
+})
